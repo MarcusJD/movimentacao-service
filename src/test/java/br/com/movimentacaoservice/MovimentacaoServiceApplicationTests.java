2413781bfdc07c54af25260f2e81f8b0e67289ab
DESKTOP-NN0ISCH\Marcus <dummy@gmail.com>
2023-07-29 20:48:34 -0300

    Initial commit.

---
+++ src/test/java/br/com/movimentacaoservice/MovimentacaoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.movimentacaoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovimentacaoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
